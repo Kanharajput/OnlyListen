b55d7228784cd33c3d8310a6d4ece06d561d8766
Kanha Tomar <kanharajput@gmail.com>
2022-09-24 13:49:35 +0530

    initial-commit -> project started

---
+++ OnlyListen/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for OnlyListen project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'OnlyListen.settings')
+
+application = get_asgi_application()
