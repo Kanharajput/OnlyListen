3c597d0687e413802835715e6725b7bac71fc11d
Kanha Tomar <kanharajput@gmail.com>
2022-09-29 11:30:16 +0530

    every page have it's own audio file

--- Home/views.py
+++ Home/views.py
@@ -1,4 +1,5 @@
 from distutils.command.upload import upload
+from pathlib import Path
 from xml.dom.minidom import Document
 from django.http import HttpResponse
 from django.shortcuts import render
@@ -7,19 +8,35 @@ import pyttsx3
 
 # Create your views here.
 def index(request):
-
-    engine = pyttsx3.init()
-    voices = engine.getProperty('voices')
-    voice = voices[2]
-    engine.setProperty('voice',voice.id)
+    page_no = 1
+    ## initialize the pyttsx3 object
+    engine =  initializeEngine()
     
     if(request.method =="POST"):
         uploaded_file = request.FILES['document']
+
+        ## open the file to read 
         with pdfplumber.open(uploaded_file) as pdf:
-            page = pdf.pages[0]
-            content = page.extract_text()
-            print(content)
-            engine.save_to_file(content,"static/upload/Trial.mp3")
-            engine.runAndWait()
+
+            # getting name of the file without extension
+            file_name = 'static/upload/%s' % uploaded_file
+            file_name = Path(file_name).stem
+
+            for page in pdf.pages:
+                content = page.extract_text()
+                print(content)
+                engine.save_to_file(content,"static/upload/"+file_name+str(page_no)+".mp3")
+                engine.runAndWait()
+                ## there are no increment decrement operator in python 
+                page_no = page_no + 1
 
     return render(request,'index.html')
+
+## initializing the engine
+def initializeEngine():
+    engine = pyttsx3.init()
+    voices = engine.getProperty('voices')
+    ## get thee voice of Hazel
+    voice = voices[3]
+    engine.setProperty('voice',voice.id)
+    return engine
