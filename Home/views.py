b1d6c072fee8453e7b1d1c9672559c21cf407a6b
Kanha Tomar <kanharajput91@gmail.com>
2023-01-21 10:31:44 +0530

    shift from pyttsx3 to gtts

--- Home/views.py
+++ Home/views.py
@@ -3,18 +3,18 @@ from pathlib import Path
 from xml.dom.minidom import Document
 from django.http import HttpResponse
 from django.shortcuts import render
+from gtts import gTTS
 import pdfplumber
-import pyttsx3 
+#import pyttsx3 
 
 # Create your views here.
 def index(request):
     page_no = 1
-    ## initialize the pyttsx3 object
-    engine =  initializeEngine()
+    # Language in which you want to convert
+    language = 'en'
     
     if(request.method =="POST"):
         uploaded_file = request.FILES['document']
-
         ## open the file to read 
         with pdfplumber.open(uploaded_file) as pdf:
 
@@ -25,18 +25,12 @@ def index(request):
             for page in pdf.pages:
                 content = page.extract_text()
                 print(content)
-                engine.save_to_file(content,"static/upload/"+file_name+str(page_no)+".mp3")
-                engine.runAndWait()
+                # change library to gtts as pyttsx3 is depend on system speakers 
+                # later in future we have to change gTTS also due to it's slow operational speed
+                audioData = gTTS(text=content, lang=language, slow=False)
+
+                audioData.save("static/upload/"+file_name+str(page_no)+".mp3")
                 ## there are no increment decrement operator in python 
                 page_no = page_no + 1
 
     return render(request,'index.html')
-
-## initializing the engine
-def initializeEngine():
-    engine = pyttsx3.init()
-    voices = engine.getProperty('voices')
-    ## get thee voice of Hazel
-    voice = voices[3]
-    engine.setProperty('voice',voice.id)
-    return engine
